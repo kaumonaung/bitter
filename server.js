6497a6e15a63bd70029c16e0d64c21c60ba7bd5c
kaumonaung <ag.kaumon@gmail.com>
2021-01-23 15:56:24 +0100

    Connect to database

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+
+// Connect to database
+connectDB();
+
+// Init middleware
+app.use(express.json());
+
+// Routes
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server running'));
