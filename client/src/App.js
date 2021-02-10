ac1856e06ef79052a1e45af5d1fc272b63b349b8
kaumonaung <ag.kaumon@gmail.com>
2021-02-10 20:01:50 +0100

    Initialized global styles, theme, favicon, and dark mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { GlobalStyle } from './global';
+import { ThemeProvider } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { useTheme } from './theme';
+import { Button, Grid, TextField } from '@material-ui/core';
 
 export const App = () => {
-  return <h1>Hello, World!</h1>;
+  const theme = useTheme();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <CssBaseline />
+
+      <Grid
+        container
+        direction='column'
+        justify='center'
+        alignItems='center'
+        spacing={5}
+      >
+        <Grid item>
+          <h1>Hello, World!</h1>
+        </Grid>
+        <Grid item>
+          <Button variant='contained' color='primary'>
+            Button #1
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant='outlined' color='primary'>
+            Button #2
+          </Button>
+        </Grid>
+        <Grid item>
+          <TextField id='outlined-basic' label='Email' variant='outlined' />
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ client/src/theme.js
@@ -0,0 +1,25 @@
+import { useMemo } from 'react';
+import { createMuiTheme } from '@material-ui/core';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
+export function useTheme() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  return useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+
+          primary: {
+            main: '#E63850',
+          },
+
+          background: {
+            default: prefersDarkMode ? '#000' : '#fff',
+          },
+        },
+      }),
+    [prefersDarkMode]
+  );
+}
